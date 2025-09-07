a86fb91240068b7300a93f9d46419d4683b341e1
awessel-git <antonw.dev@gmail.com>
2025-09-07 22:36:20 +0200

    Add TrophiesRepository class

---
+++ TrophyLibrary/TrophiesRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace TrophyLibrary;
+
+public class TrophiesRepository
+{
+    public List<Trophy> Trophies { get; set; }
+
+    public TrophiesRepository()
+    {
+        Trophies = [
+            new Trophy("Powerlifting", 2003), new Trophy("Chess", 1999), 
+            new Trophy("Swimming", 2011), new Trophy("Dancing", 2010), 
+            new Trophy("Kart racing", 1996)];
+    }
+}
