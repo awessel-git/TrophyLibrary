d8a3f2213cb8fa8735cbe1348d2cf814e869361e
Anton Wessel <a.wessel.dev@gmail.com>
2025-09-07 15:30:35 +0200

    Add Trophy class

--- TrophyLibrary/Trophy.cs
+++ TrophyLibrary/Trophy.cs
@@ -2,6 +2,42 @@
 
 public class Trophy
 {
-    public int Id { get; private set; }
+    public int Id { get; set; } // There should be a better way of doing this
 
+    private string _competition;
+    public string Competition
+    {
+        get { return _competition; }
+        set
+        {
+            if (value.Length < 3)
+                throw new ArgumentException("Competition has to be at least 3 characters");
+            else if (value == null)
+                throw new NullReferenceException("Competition cannot be null");
+            else
+                _competition = value;
+        }
+    }
+
+    private int _year;
+    public int Year
+    {
+        get { return _year; }
+        set
+        {
+            if (value >= 1970 && value <= 2025)
+                _year = value;
+            else
+                throw new ArgumentOutOfRangeException("Year has to be between 1970 and 2025");
+        }
+    }
+
+    public Trophy(int id, string competition, int year)
+    {
+        Id = id;
+        Competition = competition;
+        Year = year;
+    }
+
+    public override string ToString() => $"Trophy {Id} won in {Year}. Description: {Competition}";
 }
